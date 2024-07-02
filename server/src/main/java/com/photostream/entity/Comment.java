536c67518aee34b07bdf85900a66e57120da99b0
kinozukna <ppig4016@gmail.com>
2024-07-03 00:28:53 +0300

    Configure models to create database tables

--- server/src/main/java/com/photostream/entity/Comment.java
+++ server/src/main/java/com/photostream/entity/Comment.java
@@ -1,16 +1,28 @@
 package com.photostream.entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(nullable = false)
     private String userName;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    public Comment() {}
 
     @PrePersist
     protected void onCreate() {
--- server/src/main/java/com/photostream/entity/User.java
+++ server/src/main/java/com/photostream/entity/User.java
@@ -1,26 +1,48 @@
 package com.photostream.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.photostream.entity.enums.ERole;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    @Column(unique = true, updatable = false)
     private String userName;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {}
 
     @PrePersist
     protected void onCreate() {
