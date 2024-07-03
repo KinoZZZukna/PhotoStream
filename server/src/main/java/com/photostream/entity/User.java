6c79f6c76115ccc036fe176192eff5a0526a5ab0
kinozukna <ppig4016@gmail.com>
2024-07-04 02:21:29 +0300

    Make User implement UserDetails

--- server/src/main/java/com/photostream/entity/User.java
+++ server/src/main/java/com/photostream/entity/User.java
@@ -5,13 +5,14 @@ import com.photostream.entity.enums.ERole;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -42,10 +43,44 @@ public class User {
     @Transient
     private Collection<? extends GrantedAuthority> authorities;
 
-    public User() {}
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
 
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
