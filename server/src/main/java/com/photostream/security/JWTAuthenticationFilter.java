ed8af05bde50fe16e325d875d5477ddb364ee57b
kinozukna <ppig4016@gmail.com>
2024-07-04 15:45:32 +0300

    Implemented JWTAuthenticationFilter

---
+++ server/src/main/java/com/photostream/security/JWTAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.photostream.security;
+
+import com.photostream.entity.User;
+import com.photostream.services.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    public static final Logger logger = LoggerFactory.getLogger(JWTProvider.class);
+
+    private final JWTProvider jwtProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public JWTAuthenticationFilter(JWTProvider jwtProvider, CustomUserDetailsService customUserDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJWTFromRequest(request);
+
+            if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+                Long userId = jwtProvider.getUserIdFromJWT(jwt);
+                User userDetails = customUserDetailsService.loadUserById(userId);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, Collections.emptyList()
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Could not set user authentication in security context", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJWTFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(SecurityConstants.HEADER_STRING);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            return bearerToken.split(" ")[1];
+        }
+
+        return null;
+    }
+}
