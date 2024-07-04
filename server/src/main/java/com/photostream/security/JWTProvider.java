931371c200232d5fa3f7109709911dd12935254c
kinozukna <ppig4016@gmail.com>
2024-07-04 15:19:24 +0300

    Implemented JWTProvider

---
+++ server/src/main/java/com/photostream/security/JWTProvider.java
@@ -0,0 +1,66 @@
+package com.photostream.security;
+
+import com.photostream.entity.User;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JWTProvider {
+    public static final Logger logger = LoggerFactory.getLogger(JWTProvider.class);
+
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("id", userId);
+        claimsMap.put("username", user.getEmail());
+        claimsMap.put("firstName", user.getFirstName());
+        claimsMap.put("lastName", user.getLastName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claimsMap)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+            logger.error("Invalid JWT token");
+
+            return false;
+        }
+    }
+
+    public long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.SECRET)
+                .build()
+                .parseSignedClaims(token)
+                .getBody();
+
+        String id = (String) claims.get("id");
+
+        return Long.parseLong(id);
+    }
+}
